b56a3de95936522e4122ae2893c031f0d599d73b
AamirMajeed570 <aamirmajeedkhan570@gmail.com>
2025-05-08 10:21:07 +0530

    Add Multi Agent

---
+++ greeting_agent/agent.py
@@ -0,0 +1,11 @@
+from google.adk.agents import Agent
+
+root_agent = Agent(
+    name = "greeting_agent",
+    model = "gemini-2.0-flash",
+    description= "Greeting Agent",
+    instruction= """
+    You are helpful assistant that greets the user.
+    Ask for the user name and greet with them  by name
+    """
+)
--- multi_tool_agent/agent.py
+++ multi_tool_agent/agent.py
@@ -1,8 +1,52 @@
 import datetime
+import requests
+import fitz
+import pyttsx3
 from zoneinfo import ZoneInfo
 from google.adk.agents import Agent
 from typing import Union
 
+def read_pdf_from_url(pdf_url):
+    # Step 1: Download PDF from URL
+    response = requests.get(pdf_url);
+    if response.status_code != 200:
+        print("Failed to Fetch PDF");
+        return;
+    # Step 2: Load PDF into Memory
+    pdf_data = response.content
+    pdf_doc = fitz.open(stream=pdf_data,filetype="pdf")
+    
+    # Step 3: Extract Data
+    full_text = ""
+    for page_num in range(len(pdf_doc)):
+        page = pdf_doc[page_num]
+        full_text += page.get_text()
+        
+    pdf_doc.close()
+    return full_text
+
+def speak_text(text):
+    engine = pyttsx3.init()
+    engine.setProperty('rate',150)
+    engine.setProperty('volume',0.9)
+    engine.say(text)
+    engine.runAndWait()
+
+def read_text(url:str) -> dict:
+    """Read out the PDF for User A user will provide the Link for PDF.
+    
+    Args:
+        pdf_url: This will be the Link of the PDF
+    
+    """
+    text = read_pdf_from_url(url)
+    if text:
+        speak_text(text[:1000])
+    return {
+            "status": "success",
+            "report": f"Reading the first 1000 characters from the PDF at {url}"
+        }
+        
 def get_sum(a: float, b: float) -> dict:
     """Retreives the Sum of two Numbers.
     
@@ -80,5 +124,5 @@ root_agent = Agent(
     instruction=(
         "You are a helpful agent who can answer user questions about the time and weather in a city."
     ),
-    tools=[get_weather, get_current_time,get_sum],
+    tools=[get_weather, get_current_time,get_sum,read_text],
 )
